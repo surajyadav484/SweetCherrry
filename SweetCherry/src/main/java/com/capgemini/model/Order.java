cd2d5ae1affe9502e6d344b29a7f5a5c28365e04
manishapal145 <manishapal145@gmail.com>
2021-04-25 12:49:45 +0530

    Initial Commit

---
+++ SweetCherry/src/main/java/com/capgemini/model/Order.java
@@ -0,0 +1,108 @@
+package com.capgemini.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ORDER_ID" ,nullable = false)
+	private int orderId;
+	
+	@Column(name = "ORDER_DATE" ,nullable = false)
+	private String orderDate;
+	
+	@Column(name = "QUANTITY" ,nullable = false)
+	private int quantity;
+	
+	@Column(name = "ORDER_STATUS" )
+	private String orderStatus;
+	
+	@Column(name = "TOTAL_PRICE")
+	private double totalPrice;
+	
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private UserDetails roleDetails;
+
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "cupcakeDetails_orders", joinColumns = { @JoinColumn(name = "order_id") }, inverseJoinColumns = { @JoinColumn(name = "cupcake_id") })
+	private CupcakeDetails cupcakeDetails;
+	
+	@OneToOne
+	@JoinColumn(name = "status")
+	private Payment payment;
+	
+	
+	public Order() {
+		super();
+	}
+	
+	public Order(int orderId, String orderDate, int quantity, String orderStatus, UserDetails roleDetails,
+			Payment payment) {
+		super();
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+		this.quantity = quantity;
+		this.orderStatus = orderStatus;
+		this.roleDetails = roleDetails;
+		this.payment = payment;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public UserDetails getRoleDetails() {
+		return roleDetails;
+	}
+
+	public void setRoleDetails(UserDetails roleDetails) {
+		this.roleDetails = roleDetails;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
+	
+}
---
+++ SweetCherry/src/main/java/com/capgemini/model/Payment.java
@@ -0,0 +1,115 @@
+package com.capgemini.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Payment {
+
+			
+		 @Id
+		 @Column(name = "CARD_NUMBER", length = 12 ,nullable = false)
+		 private long cardNo;
+		 
+		 @Column(name = "CVV",nullable = false)
+		 private int cvv;
+		 
+		 @Column(name = "CARD_HOLDER_NAME" , nullable = false)
+		 private String cardHolderName;
+		 
+		 @Column(name = "EXPIRY_DATE" ,nullable = false)
+		 private String expiryDate;
+		 
+		 @Column(name = "STATUS", nullable = false)
+		 private String status;
+		 
+		 @Column(name = "CASH_ON_DELIVERY")
+		 private boolean cod;
+		 
+		 @OneToMany
+		 @JoinColumn(name="userId")
+		 private UserDetails roleDetails;
+		 
+		 public String getStatus() {
+			return status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public boolean isCod() {
+			return cod;
+		}
+
+		public void setCod(boolean cod) {
+			this.cod = cod;
+		}
+
+		public UserDetails getRoleDetails() {
+			return roleDetails;
+		}
+
+		public void setRoleDetails(UserDetails roleDetails) {
+			this.roleDetails = roleDetails;
+		}
+
+		public Payment() {
+		}
+
+		public Payment(long cardNo, int cvv, String cardHolderName, String expiryDate, String paymentStatus) {
+			super();
+			this.cardNo = cardNo;
+			this.cvv = cvv;
+			this.cardHolderName = cardHolderName;
+			this.expiryDate = expiryDate;
+			this.status = paymentStatus;
+		}
+
+		public long getCardNo() {
+			return cardNo;
+		}
+
+		public void setCardNo(long cardNo) {
+			this.cardNo = cardNo;
+		}
+
+		public int getCvv() {
+			return cvv;
+		}
+
+		public void setCvv(int cvv) {
+			this.cvv = cvv;
+		}
+
+		public String getCardHolderName() {
+			return cardHolderName;
+		}
+
+		public void setCardHolderName(String cardHolderName) {
+			this.cardHolderName = cardHolderName;
+		}
+
+		public String getExpiryDate() {
+			return expiryDate;
+		}
+
+		public void setExpiryDate(String expiryDate) {
+			this.expiryDate = expiryDate;
+		}
+
+		public String getPaymentStatus() {
+			return status;
+		}
+
+		public void setPaymentStatus(String paymentStatus) {
+			this.status = paymentStatus;
+		}
+		 
+		 
+
+}
