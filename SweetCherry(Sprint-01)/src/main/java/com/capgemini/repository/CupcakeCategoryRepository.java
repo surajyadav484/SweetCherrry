04853271f7e77c7da4f7eec35c893534e483436d
surajyadav484 <83104958+surajyadav484@users.noreply.github.com>
2021-05-03 16:56:16 +0530

    Delete SweetCherry(Sprint-01) directory

--- SweetCherry(Sprint-01)/src/main/java/com/capgemini/repository/CupcakeCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.model.CupcakeCategory;
-
-public interface CupcakeCategoryRepository extends JpaRepository<CupcakeCategory, Integer> {
-
-}
